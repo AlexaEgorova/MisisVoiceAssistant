a6ba2570c135ce0a14264709110489bbe0b8bc8b
AlexaEgorova <egorova.a007@gmail.com>
2022-10-31 20:22:16 +0300

    init structure

---
+++ bot.py
@@ -0,0 +1,40 @@
+
+def chatter():
+    pass
+
+
+def recognizer():
+    pass
+
+
+def talker():
+    pass
+
+
+def converter():
+    pass
+
+
+def prepare_text():
+    pass
+
+
+class MVABot:
+
+    def __init__():
+        pass
+
+    def _tg_callback_start():
+        pass
+
+    def _tg_callback_voice():
+        pass
+        
+    def _tg_handle_voice():
+        pass
+
+    def start():
+        pass
+
+if __name__ == "__main__":
+    pass
